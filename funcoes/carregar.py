69198b79626282326917ef39b733a85768e8bfba
fernandobueno88-dev <fernandobueno88@gmail.com>
2026-07-01 10:37:40 -0300

    Update carregar.py

--- funcoes/carregar.py
+++ funcoes/carregar.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import os
 
+PASTA_DADOS = "dados"
+
+def carregar_macros():
+    arquivo = os.path.join(PASTA_DADOS, "Relatório de Macro.xlsx")
+    return pd.read_excel(arquivo)
+
+def carregar_viagens():
+    arquivo = os.path.join(PASTA_DADOS, "Controle de Produção.xlsx")
+    return pd.read_excel(arquivo, sheet_name="Viagens")
